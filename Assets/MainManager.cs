26e11905c7c53dac88e7d919428a6ddf214c3c4e
nuttakit khongkaew <155617571+Supersonixz@users.noreply.github.com>
2024-04-16 20:39:04 +0700

    init

---
+++ Assets/MainManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public GameObject mainCanvas;
+    public GameObject logCanvas;
+    public GameObject actionCanvas;
+    public GameObject uiCanvas;
+    public GameObject loadingCanvas;
+    
+    void Start()
+    {
+      //  loadingCanvas.GetComponent<LoadingManager>
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
