2628d02350f37ae12ff3a1840d9e649d397478c9
MarojT <celta0992@hotmail.com>
2024-04-18 18:23:59 +0700

    Add damage over time.

--- Assets/RunningFeature/Scripts/PlayerBehavior.cs
+++ Assets/RunningFeature/Scripts/PlayerBehavior.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     [SerializeField] HealthBar _healthbar;
+    private int dot = 1; //dot = damage over time
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,8 @@ public class PlayerBehavior : MonoBehaviour
         {
             PlayerHeal(20);
         }
+
+        PlayerTakeDmg(Time.deltaTime * dot);
         
     }
 
